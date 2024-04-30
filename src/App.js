90627ab30b636997ed47dc0776beb0346832428e
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-04-30 16:05:25 -0300

    feat: Página principal do projeto buscando json de uma API com requisições http

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+//https://sujeitoprogramador.com/rn-api/?api=posts
+import './style.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+
+    function loadApi() {
+      let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+
+      fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        console.log(json);
+        setNutri(json);
+      })
+    }
+
+    loadApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <strong>React Nutri</strong>
       </header>
+
+      {nutri.map((item) => {
+        return(
+          <article key={item.id} className='post'>
+            <strong className='titulo'>{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className='capa' ></img>
+            <p className='subtitulo'>
+              {item.subtitulo}
+            </p>
+            <a className='botao'>Acessar</a>
+          </article>
+        )
+      })}
     </div>
   );
 }
